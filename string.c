40d6e87565316892420cedc45db0cbf0408c65fa
Paul Su <pasu@actiontec.com>
2016-09-30 18:25:23 -0700

    Change return type of my_strlen to size_T

---
+++ string.c
@@ -0,0 +1,28 @@
+#include<stdio.h>
+
+size_t my_strlen(char* s)
+{
+   char* p=s;
+   
+   while(*p) 
+       ++p;
+
+   return (p-s);
+}
+
+int main()
+{
+   int i;
+   char* s[]= {
+           "Git tutorials",
+           "Tutorials Point"
+          };
+
+   for (int i=0;i<2;i++)
+   {
+       printf("string length of %s =%d\n", s[i], my_strlen(s[i]));
+   }
+
+   return 0;
+
+}
